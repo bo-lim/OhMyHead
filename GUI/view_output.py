393dc89b963c9f819607e04294e764d6122c8e0b
bbh-pharm <bbh9955@gmail.com>
2022-01-18 01:39:50 +0900

    Convert GUI view_output.py like a module

--- GUI/view_output.py
+++ GUI/view_output.py
@@ -1,42 +1,46 @@
+import os
 import sys
-import urllib.request
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5 import uic, QtGui 
-import torch # For model's output
+import torch 
+
+### PATH ###
+# current_path = ./OHMYHAIR
+current_path = os.getcwd()
+print(current_path)
 
 ### Connect to ui file ###
-form_class = uic.loadUiType("output.ui")[0]
-
-### Prediction of model ###
-prediction_vec = torch.Tensor([0.1864, 0.4095, 0.4885, 0.0716, 0.5891, 0.2249]) # it's a temp prediction... (needed to be amended)
-prediction_list = ['미세각질', '피지과다', '모낭사이홍반', '모낭홍반농포', '비듬', '탈모']
-prediction = prediction_list[torch.argmax(prediction_vec)]
+form_class = uic.loadUiType(os.path.join(current_path, "GUI/output.ui"))[0]
 
 ### Disease description ###
 description = []    
-with open("disease_explained.txt", 'r') as f:
+with open(os.path.join(current_path, "GUI/disease_explained.txt"), 'r') as f:
     for line in f:
         description.append(line)
 
 
 ### GUI Output ###
 class WindowClass(QMainWindow, form_class) :
-    def __init__(self) :
+    def __init__(self, image_path, pred_tensor) :
         super().__init__()
         self.setupUi(self)
+        ### Prediction of model ###
+        prediction_vec = pred_tensor
+        prediction_list = ['미세각질', '피지과다', '모낭사이홍반', '모낭홍반농포', '비듬', '탈모']
+        prediction = prediction_list[torch.argmax(prediction_vec)]
 
         # Set the window title
         self.setWindowTitle("Oh My Head - CUAI winter project - Healthcare team")
 
         # Set size of window
         # self.setFixedSize(1280, 600)
-        self.setFixedSize(1400, 700)
+        self.setFixedSize(1400, 710)
 
         # Title image
         self.qPixmapFileVar_title = QPixmap()
-        self.qPixmapFileVar_title.load("logo_output.png")
+        self.qPixmapFileVar_title.load(os.path.join(current_path, 'GUI/logo_output.png'))
         self.qPixmapFileVar_title = self.qPixmapFileVar_title.scaledToWidth(200)
         self.title_box.setPixmap(self.qPixmapFileVar_title)
 
@@ -46,7 +50,7 @@ class WindowClass(QMainWindow, form_class) :
 
         # Scalp image
         self.qPixmapFileVar_scalp = QPixmap()
-        self.qPixmapFileVar_scalp.load("my_scalp.jpg")
+        self.qPixmapFileVar_scalp.load(os.path.join(current_path, image_path))
         self.qPixmapFileVar_scalp = self.qPixmapFileVar_scalp.scaledToWidth(300)
         self.my_scalp.setPixmap(self.qPixmapFileVar_scalp)
 
@@ -68,27 +72,23 @@ class WindowClass(QMainWindow, form_class) :
         ## Survey figures
         # shampoo
         self.qPixmapFileVar_title2 = QPixmap()
-        self.qPixmapFileVar_title2.load(f"figure/{prediction}_샴푸사용빈도.png")
+        self.qPixmapFileVar_title2.load(os.path.join(current_path, f"GUI/figure/{prediction}_샴푸사용빈도.png"))
         self.qPixmapFileVar_title2 = self.qPixmapFileVar_title2.scaledToWidth(720)
         self.shampoo_figure.setPixmap(self.qPixmapFileVar_title2)
         # perm
         self.qPixmapFileVar_title3 = QPixmap()
-        self.qPixmapFileVar_title3.load(f"figure/{prediction}_펌주기.png")
+        self.qPixmapFileVar_title3.load(os.path.join(current_path, f"GUI/figure/{prediction}_펌주기.png"))
         self.qPixmapFileVar_title3 = self.qPixmapFileVar_title3.scaledToWidth(720)
         self.perm_figure.setPixmap(self.qPixmapFileVar_title3)
         # dye
         self.qPixmapFileVar_title4 = QPixmap()
-        self.qPixmapFileVar_title4.load(f"figure/{prediction}_염색주기.png")
+        self.qPixmapFileVar_title4.load(os.path.join(current_path, f"GUI/figure/{prediction}_염색주기.png"))
         self.qPixmapFileVar_title4 = self.qPixmapFileVar_title4.scaledToWidth(720)
         self.dye_figure.setPixmap(self.qPixmapFileVar_title4)
 
         
-
-if __name__ == "__main__" :
+def main(image_path, pred_tensor):
     app = QApplication(sys.argv) 
-
-    myWindow = WindowClass() 
-
+    myWindow = WindowClass(image_path, pred_tensor)
     myWindow.show()
-
     app.exec_()
